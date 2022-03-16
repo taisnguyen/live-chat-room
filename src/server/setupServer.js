5cb8198fac0160eb2a0f4ec9bd588d6df7cc13b5
Tai Nguyen <taisnguyen123@yahoo.com>
2022-03-15 23:27:19 -0400

    added process.env.PORT

--- src/server/setupServer.js
+++ src/server/setupServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 // Setup server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 app.use(express.static(path.join(__dirname, "../../public")));
